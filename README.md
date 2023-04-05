39196cfe3c03b02b9fb08ba51409f9ca48be59bd
Bhavik MacBook PRO 16 <bhavik@ingressit.com>
2023-04-05 13:26:37 +0530

    First Release

---
+++ README.md
@@ -0,0 +1,117 @@
+### Simplify code reviews, just say `openai`!
+
+# OpenAI Code Review
+
+A Gitea Action that uses OpenAI to analyze code in pull request comments.
+
+## Usage
+
+To use this action in your workflow, add the following step:
+
+```yaml
+name: OpenAI Code Review
+uses: bhavik/gitea-code-review-action@v0.1
+with:
+    PROGRAMMING_LANGUAGE: 'JavaScript'
+    REVIEW_COMMENT_PREFIX: 'crai:'
+    FULL_REVIEW_COMMENT: 'crai'
+    OPENAI_TOKEN: ${{ secrets.OPENAI_TOKEN }}
+    GITHUB_TOKEN: ${{ secrets.GH_TOKEN }}
+```
+
+## Inputs
+
+This action accepts the following inputs:
+
+- `PROGRAMMING_LANGUAGE` (optional): The programming language of the code in the GitHub repository. If not provided, the detected programming language will be used.
+- `OPENAI_TOKEN` (required): The API token for the OpenAI ChatGPT API.
+- `GITHUB_TOKEN` (required): The API token for the Github API.
+- `FULL_REVIEW_COMMENT` (required): The comment to trigger code review for the pull request.
+- `REVIEW_COMMENT_PREFIX` (required): The comment prefix to trigger code review with the comment.
+- `GITHUB_BASE_URL` (optional): The base URL for the GitHub API.
+- `MAX_CODE_LENGTH` (optional): The maximum code length for the pull request code to be sent to OpenAI. The default value is 6000.
+- `PROMPT_TEMPLATE` (optional): The template for the FULL_REVIEW_COMMENT prompt. The default value is:
+```
+Please analyze the pull request's code and inform me whether it requires optimization, and provide an explanation for your decision:
+
+${code}
+```
+- `ANSWER_TEMPLATE` (optional): The template for the answer sent to the GitHub comment. The default value is:
+```
+AI Code Review:
+
+${answer}
+```
+
+## Outputs
+
+This action does not produce any outputs.
+
+## Example workflow
+
+Here's an example workflow that uses this action to analyze code in pull request comments:
+
+```yaml
+name: Code Review
+
+on:
+  issue_comment:
+    types: [created, edited]
+
+jobs:
+  code-review:
+    runs-on: ubuntu-latest
+    if: github.event.comment.user.login == 'bhavik' && startsWith(github.event.comment.body, 'openai')
+    steps:
+    - name: OpenAI Code Review
+      uses: bhavik/gitea-code-review-action@v0.1
+      with:
+        PROGRAMMING_LANGUAGE: 'JavaScript'
+        OPENAI_TOKEN: ${{ secrets.OPENAI_TOKEN }}
+        GITHUB_TOKEN: ${{ secrets.GH_TOKEN }}
+        FULL_REVIEW_COMMENT: 'openai'
+        REVIEW_COMMENT_PREFIX: 'openai:'
+```
+
+This workflow runs the `OpenAI Code Review` action when a pull request comment is created or edited. The action uses the `PROGRAMMING_LANGUAGE`, `FULL_REVIEW_COMMENT`, `REVIEW_COMMENT_PREFIX`, `OPENAI_TOKEN` and `GITHUB_TOKEN` input values to analyze the code in the pull request comment.
+
+You can also trigger github action when a pull request opened.
+
+## Github
+
+### Code Review for PR
+
+If a comment exactly matches `openai`, a code review will be triggered for the pull request's diff.
+
+Note that only the first 6000 characters of code will be used due to the token limit of the OpenAI API.
+
+### Code Review for a specific file
+
+If a comment starts with `openai:`, you can use it to communicate with ChatGPT just like you would in the official interface.
+
+To reference the diff code of the pull request in your comment, use ${code}:
+
+```
+openai: Please explain the usage of the `getPrCode` function:
+
+${code}
+```
+
+To reference the diff code of a specific file in the pull request, use ${file:filename}:
+
+```
+openai: Can you check if there are any bugs in the following code?
+
+${file:dist/index.js}
+```
+
+## Screenshots
+
+
+## More Examples
+
+
+
+## License
+
+The code in this repository is licensed under the MIT license. See `LICENSE` for more information.
---
+++ index.js
@@ -0,0 +1,169 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios');
+const detect = require('language-detect');
+const httpsProxyAgent = require('https-proxy-agent');
+
+function configWithProxy(config) {
+    var c = config || {};
+    if (process.env.OPENAI_PROXY) {
+        core.debug(`use proxy: ${process.env.OPENAI_PROXY}`);
+        c.proxy = false;
+        c.httpsAgent = new httpsProxyAgent(process.env.OPENAI_PROXY);
+        return c;
+    }
+    return c;
+}
+
+async function run() {
+  try {
+    // Get input values
+    const programmingLanguage = core.getInput('PROGRAMMING_LANGUAGE');
+    const openaiToken = core.getInput('OPENAI_TOKEN');
+    const fullReviewComment = core.getInput('FULL_REVIEW_COMMENT');
+    const reviewCommentPrefix = core.getInput('REVIEW_COMMENT_PREFIX');
+    const githubToken = core.getInput('GITHUB_TOKEN');
+    const githubBaseURL = core.getInput('GITHUB_BASE_URL') || process.env.GITHUB_API_URL;
+    const promptTemplate = core.getInput('PROMPT_TEMPLATE');
+    const maxCodeLength = core.getInput('MAX_CODE_LENGTH');
+    const answerTemplate = core.getInput('ANSWER_TEMPLATE');
+
+    core.debug(`programmingLanguage: ${programmingLanguage}`);
+    core.debug(`openaiToken length: ${openaiToken.length}`);
+    core.debug(`fullReviewComment: ${fullReviewComment}`);
+    core.debug(`reviewCommentPrefix: ${reviewCommentPrefix}`);
+    core.debug(`githubToken length: ${githubToken.length}`);
+    core.debug(`githubBaseURL: ${githubBaseURL}`);
+    core.debug(`promptTemplate: ${promptTemplate}`);
+    core.debug(`maxCodeLength: ${maxCodeLength}`);
+    core.debug(`answerTemplate: ${answerTemplate}`);
+
+    // Get information about the pull request review
+    const comment = github.context.payload.comment;
+    const repoName = github.context.payload.repository.name;
+    const repoOwner = github.context.payload.repository.owner.login;
+    const prNumber = github.context.payload.number || github.context.payload.issue.number; // get number from a pull request event or comment event
+
+    // Get the code to analyze from the review comment 
+    var content = comment && comment.body || "";
+
+    const url = `${githubBaseURL}/${repoOwner}/${repoName}/pulls/${prNumber}`;
+    core.debug(`diff url: ${url}`);
+    var response = await axios.get(url, {
+        headers: {
+            Authorization: `Bearer ${githubToken}`,
+            Accept: 'application/vnd.github.diff'
+        }
+    });
+    const code = response.data;
+    core.debug(`diff code: ${code}`);
+    const files = parsePullRequestDiff(code);
+    core.debug(`diff files: ${files}`);
+
+    if (!content || content == fullReviewComment) {
+        // Extract the code from the pull request content
+        content = promptTemplate.replace('${code}', code);
+    } else {
+        content = content.substring(reviewCommentPrefix.length);
+        content = content.replace('${code}', code);
+        const fileNames = findFileNames(content);
+        core.debug(`found files name in commment: ${fileNames}`);
+        for (const fileName of fileNames) {
+            for (const key of Object.keys(files)) {
+                if (key.includes(fileName)) {
+                    core.debug(`replace \${file:${fileName}} with ${key}'s diff`);
+                    content = content.replace(`\${file:${fileName}}`, files[key]);
+                    break;
+                }
+            }
+        }
+    }
+    content = content.substring(0, maxCodeLength);
+
+    // Determine the programming language if it was not provided
+    if (programmingLanguage == 'auto') {
+        const detectedLanguage = detect(code);
+        core.debug(`Detected programming language: ${detectedLanguage}`);
+        programmingLanguage = detectedLanguage;
+    }
+
+    var messages = [{
+        role: "system",
+        content: `You are a master of programming language ${programmingLanguage}`
+    }, {
+        role: "user",
+        content: content
+    }];
+
+    core.debug(`content: ${content}`);
+
+    // Call the OpenAI ChatGPT API to analyze the code
+    response = await axios.post('https://api.openai.com/v1/chat/completions', {
+        "model": "gpt-3.5-turbo",
+        "messages": messages
+    }, configWithProxy({
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${openaiToken}`
+      }
+    }));
+
+    const answer = response.data.choices[0].message.content;
+    core.debug(`openai response: ${answer}`);
+
+    // Reply to the review comment with the OpenAI response
+    const octokit = github.getOctokit(githubToken, {
+        baseUrl: githubBaseURL
+    });
+    await octokit.rest.issues.createComment({
+        owner: repoOwner,
+        repo: repoName,
+        issue_number: prNumber,
+        body: answerTemplate.replace('${answer}', answer)
+        // in_reply_to: comment.id
+      });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+function parsePullRequestDiff(diffContent) {
+    const files = {};
+    const diffLines = diffContent.split('\n');
+
+    let currentFile = null;
+    let currentLines = [];
+
+    for (const line of diffLines) {
+      if (line.startsWith('diff --git')) {
+        // Start of a new file
+        if (currentFile) {
+          files[currentFile] = currentLines.join('\n');
+        }
+        currentFile = line.substring('diff --git'.length + 1);
+        currentLines = [line];
+      } else {
+        // Add the line to the current file's diff
+        currentLines.push(line);
+      }
+    }
+
+    // Add the last file's diff
+    if (currentFile) {
+      files[currentFile] = currentLines.join('\n');
+    }
+
+    return files;
+}
+
+function findFileNames(str) {
+    const pattern = /\${file:([^{}]+)}/g;
+    const matches = str.matchAll(pattern);
+    const names = [];
+    for (const match of matches) {
+      names.push(match[1]);
+    }
+    return names;
+}
+
+run();
