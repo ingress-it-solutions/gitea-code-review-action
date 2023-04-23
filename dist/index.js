7a961911b394131bcbef841304e87392e20b4692
Bhavik MacBook PRO 16 <bhavik@ingressit.com>
2023-04-23 17:56:53 +0530

    Fixed issue with Gitea Pull Request URL

--- dist/index.js
+++ dist/index.js
@@ -39275,7 +39275,7 @@ async function run() {
       }
       else if(sourceAt === 'gitea')
       {
-          const url = `${githubBaseURL}/api/v1/repos/${repoOwner}/${repoName}/pulls/${prNumber}/diff`;
+          const url = `${githubBaseURL}/api/v1/repos/${repoOwner}/${repoName}/pulls/${prNumber}.diff`;
           console.log(`diff url: ${url}`);
           var response = await axios.get(url, {
               headers: {
