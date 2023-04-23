6344cae339f6c695a4f7bf1b3479e4c07fae6b0b
Bhavik MacBook PRO 16 <bhavik@ingressit.com>
2023-04-23 16:49:26 +0530

    Added code for Gitea

--- dist/index.js
+++ dist/index.js
@@ -39201,13 +39201,7 @@ function configWithProxy(config) {
     return c;
 }
 
-// Make a POST request to create a comment on a pull request
-const createCommentOnPullRequest = async (repoOwner, repoName, prNumber, comment) => {
-    const url = `${giteaBaseURL}/repos/${repoOwner}/${repoName}/issues/${prNumber}/comments`;
-    const headers = { 'Authorization': `token ${giteaToken}` };
-    const data = { 'body': comment };
-    await axios.post(url, data, { headers });
-}
+
 
 async function run() {
   try {
@@ -39362,6 +39356,13 @@ async function run() {
     {
 
         const comment = answerTemplate.replace('${answer}', answer);
+        // Make a POST request to create a comment on a pull request
+        const createCommentOnPullRequest = async (repoOwner, repoName, prNumber, comment) => {
+            const url = `${githubBaseURL}/repos/${repoOwner}/${repoName}/issues/${prNumber}/comments`;
+            const headers = { 'Authorization': `token ${githubToken}` };
+            const data = { 'body': comment };
+            await axios.post(url, data, { headers });
+        }
         await createCommentOnPullRequest(repoOwner, repoName, prNumber, comment);
     }
   } catch (error) {
